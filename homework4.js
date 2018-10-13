675539ed7225d2cb5680e7f7178b3c3d02caf5af
garabed99 <37003106+garabed99@users.noreply.github.com>
2018-10-13 17:45:51 +0400

    Update homework4.js

--- homework4.js
+++ homework4.js
@@ -1,4 +1,6 @@
-//1st
+//1st task is the video
+
+//2nd task
 const triangleStars = function(height)
 {
     const base = 2 * height - 1;
@@ -10,7 +12,7 @@ const triangleStars = function(height)
 };
 console.log(triangleStars(4));
 
-//2nd
+//3rd task
 const multiToSingle = function(array) {
     let newarray = [];
     let ParentIndex = 0;
@@ -29,7 +31,7 @@ const multiToSingle = function(array) {
 }
 console.log(multiToSingle([[20,21,22],[23,24,25]]));
 
-//3rd
+//4th task
 const findMinMax = function (array, boolean){
     if (boolean) {
         return Math.max(...array);
@@ -38,7 +40,7 @@ const findMinMax = function (array, boolean){
 };
 console.log(findMinMax([4, 2, 66, -44, 8], true));
 
-//4th
+//5th task
     const forEach = function (array){
     let i = 0;
     while (i < array.length) {
@@ -48,7 +50,7 @@ console.log(findMinMax([4, 2, 66, -44, 8], true));
 };
 console.log(forEach([15, 16, 17]));
 
-//5th
+//6th task
 const sum = function (array) {
     sumOfNum = 0;
     let i = 0;
@@ -61,7 +63,7 @@ const sum = function (array) {
 };
 console.log(sum([5, 10, 15]));
 
-//6th
+//7th task
 const reverse = function (stringArr) {
    let outcome = "";
    let i = stringArr.length - 1;
@@ -74,7 +76,7 @@ const reverse = function (stringArr) {
  };
  console.log(reverse("garo"));
  
-//7th
+//8th task
 const checkerboard = function(size) {
     let i = 0;
     while (i < size) {
