fe460e0e895404252cd6dfd0b8c555c7bac3f6d1
garabed99 <37003106+garabed99@users.noreply.github.com>
2018-10-13 17:42:10 +0400

    homework4

---
+++ homework4.js
@@ -0,0 +1,105 @@
+//1st
+const triangleStars = function(height)
+{
+    const base = 2 * height - 1;
+    let i = 0
+    while(i < height) {
+        console.log(' '.repeat(i) + '*'.repeat(base - 2 * i));
+        i++;
+    }
+};
+console.log(triangleStars(4));
+
+//2nd
+const multiToSingle = function(array) {
+    let newarray = [];
+    let ParentIndex = 0;
+    while (ParentIndex < array.length) {
+        const childArray = array[ParentIndex]
+        ParentIndex++;
+        let ChildIndex = 0;
+        while (ChildIndex < childArray.length) {
+        
+            newarray[newarray.length] = childArray[ChildIndex];
+            ChildIndex++;
+        }
+       
+    }
+    return newarray+"";
+}
+console.log(multiToSingle([[20,21,22],[23,24,25]]));
+
+//3rd
+const findMinMax = function (array, boolean){
+    if (boolean) {
+        return Math.max(...array);
+    }   
+    return Math.min(...array);
+};
+console.log(findMinMax([4, 2, 66, -44, 8], true));
+
+//4th
+    const forEach = function (array){
+    let i = 0;
+    while (i < array.length) {
+        console.log(array[i]);
+        i++;
+    }
+};
+console.log(forEach([15, 16, 17]));
+
+//5th
+const sum = function (array) {
+    sumOfNum = 0;
+    let i = 0;
+    while (i < array.length) {
+        sumOfNum = sumOfNum + array[i];
+        i++
+       
+    }
+    return sumOfNum;
+};
+console.log(sum([5, 10, 15]));
+
+//6th
+const reverse = function (stringArr) {
+   let outcome = "";
+   let i = stringArr.length - 1;
+   while (i >= 0 ){
+     outcome = outcome + stringArr[i];
+     i--;
+
+   }
+   return outcome;
+ };
+ console.log(reverse("garo"));
+ 
+//7th
+const checkerboard = function(size) {
+    let i = 0;
+    while (i < size) {
+        if ( i %2 === 0) {
+        console.log("# ".repeat(size));
+    } else { 
+    console.log(" #".repeat(size));
+        }
+    i++;
+  }
+};
+console.log(checkerboard(5));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
